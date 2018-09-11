af3e80e14eab53450be9be5dfc0482462cae3a7f
Asger <asger.nyman@alexandra.dk>
2018-09-11 10:04:53 +0200

    Test connected component

--- src/algorithms/connected_components.rs
+++ src/algorithms/connected_components.rs
@@ -6,6 +6,7 @@ use std::collections::HashSet;
 pub fn connected_components(mesh: &HalfEdgeMesh, face_id: &FaceID) -> HashSet<FaceID>
 {
     let mut component = HashSet::new();
+    component.insert(face_id.clone());
     let mut to_be_tested = vec![face_id.clone()];
 
     loop {
@@ -25,4 +26,109 @@ pub fn connected_components(mesh: &HalfEdgeMesh, face_id: &FaceID) -> HashSet<Fa
     }
     component
 
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_one_connected_component()
+    {
+        let mesh = create_connected_test_object();
+        let cc = connected_components(&mesh, &FaceID::new(0));
+        assert_eq!(cc.len(), mesh.no_faces());
+    }
+
+    #[test]
+    fn test_three_connected_component()
+    {
+        let mesh = create_unconnected_test_object();
+
+        let mut cc = connected_components(&mesh, &FaceID::new(0));
+        assert_eq!(cc.len(), 12);
+
+        cc = connected_components(&mesh, &FaceID::new(13));
+        assert_eq!(cc.len(), 2);
+
+        cc = connected_components(&mesh, &FaceID::new(14));
+        assert_eq!(cc.len(), 1);
+    }
+
+    fn create_connected_test_object() -> HalfEdgeMesh
+    {
+        let positions: Vec<f32> = vec![
+            1.0, -1.0, -1.0,
+            1.0, -1.0, 1.0,
+            -1.0, -1.0, 1.0,
+            -1.0, -1.0, -1.0,
+            1.0, 1.0, -1.0,
+            1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.0,
+            -1.0, 1.0, -1.0
+        ];
+
+        let indices: Vec<u32> = vec![
+            0, 1, 2,
+            0, 2, 3,
+            4, 7, 6,
+            4, 6, 5,
+            0, 4, 5,
+            0, 5, 1,
+            1, 5, 6,
+            1, 6, 2,
+            2, 6, 7,
+            2, 7, 3,
+            4, 0, 3,
+            4, 3, 7
+        ];
+
+        HalfEdgeMesh::create(indices, positions).unwrap()
+    }
+
+    fn create_unconnected_test_object() -> HalfEdgeMesh
+    {
+        let positions: Vec<f32> = vec![
+            1.0, -1.0, -1.0,
+            1.0, -1.0, 1.0,
+            -1.0, -1.0, 1.0,
+            -1.0, -1.0, -1.0,
+            1.0, 1.0, -1.0,
+            1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.0,
+            -1.0, 1.0, -1.0,
+
+            -1.0, 2.0, -1.0,
+            -1.0, 3.0, -1.0,
+            -2.0, 4.0, -1.0,
+            -2.0, 1.0, -1.0,
+
+            -1.0, 3.0, -2.0,
+            -2.0, 4.0, -3.0,
+            -2.0, 1.0, -4.0
+        ];
+
+        let indices: Vec<u32> = vec![
+            0, 1, 2,
+            0, 2, 3,
+            4, 7, 6,
+            4, 6, 5,
+            0, 4, 5,
+            0, 5, 1,
+            1, 5, 6,
+            1, 6, 2,
+            2, 6, 7,
+            2, 7, 3,
+            4, 0, 3,
+            4, 3, 7,
+
+            8, 9, 10,
+            8, 10, 11,
+
+            12, 13, 14
+        ];
+
+        HalfEdgeMesh::create(indices, positions).unwrap()
+    }
 }
