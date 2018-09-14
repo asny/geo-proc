587a636431aa93bb2ffd4bedf82d344062a623f1
Asger <asger.nyman@alexandra.dk>
2018-09-14 12:58:09 +0200

    Clean-up

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,15 @@ extern crate tobj;
 
 pub extern crate glm;
 
-mod attribute;
-pub mod loader;
-pub mod models;
 pub mod ids;
-mod connectivity_info;
 pub mod traversal;
-
 pub mod mesh;
 pub mod dynamic_mesh;
 pub mod static_mesh;
 
+pub mod loader;
+pub mod models;
 pub mod algorithms;
+
+mod attribute;
+mod connectivity_info;
