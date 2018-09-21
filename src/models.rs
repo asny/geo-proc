3dd8384c0aa693b8890711ec9ce59c56411d1a7a
Asger <asger.nyman@alexandra.dk>
2018-09-21 11:39:41 +0200

    Added dynamic mesh box to models

--- src/models.rs
+++ src/models.rs
@@ -1,8 +1,7 @@
 
-use static_mesh::StaticMesh;
-use mesh;
+use mesh::*;
 
-pub fn create_connected_cube() -> Result<StaticMesh, mesh::Error>
+pub fn create_connected_cube() -> Result<StaticMesh, Error>
 {
     let positions: Vec<f32> = vec![
         1.0, -1.0, -1.0,
@@ -16,25 +15,56 @@ pub fn create_connected_cube() -> Result<StaticMesh, mesh::Error>
     ];
 
     let indices: Vec<u32> = vec![
-        1, 2, 3,
-        1, 3, 4,
-        5, 8, 7,
-        5, 7, 6,
+        0, 1, 2,
+        0, 2, 3,
+        4, 7, 6,
+        4, 6, 5,
+        0, 4, 5,
+        0, 5, 1,
         1, 5, 6,
         1, 6, 2,
         2, 6, 7,
         2, 7, 3,
-        3, 7, 8,
-        3, 8, 4,
-        5, 1, 4,
-        5, 4, 8
+        4, 0, 3,
+        4, 3, 7
     ];
 
     let mesh = StaticMesh::create(indices, att!["position" => (positions, 3)])?;
     Ok(mesh)
 }
 
-pub fn create_cube() -> Result<StaticMesh, mesh::Error>
+pub fn create_cube_as_dynamic_mesh() -> Result<DynamicMesh, Error>
+{
+    let positions: Vec<f32> = vec![
+        1.0, -1.0, -1.0,
+        1.0, -1.0, 1.0,
+        -1.0, -1.0, 1.0,
+        -1.0, -1.0, -1.0,
+        1.0, 1.0, -1.0,
+        1.0, 1.0, 1.0,
+        -1.0, 1.0, 1.0,
+        -1.0, 1.0, -1.0
+    ];
+
+    let indices: Vec<u32> = vec![
+        0, 1, 2,
+        0, 2, 3,
+        4, 7, 6,
+        4, 6, 5,
+        0, 4, 5,
+        0, 5, 1,
+        1, 5, 6,
+        1, 6, 2,
+        2, 6, 7,
+        2, 7, 3,
+        4, 0, 3,
+        4, 3, 7
+    ];
+
+    Ok(DynamicMesh::create(indices, positions, None))
+}
+
+pub fn create_cube() -> Result<StaticMesh, Error>
 {
     let positions: Vec<f32> = vec![
         1.0, 1.0, -1.0,
