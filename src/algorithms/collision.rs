f90c72bfef9ca3f87aa05eb2fb983217bfbdc0e1
Asger <asger.nyman@alexandra.dk>
2018-09-25 15:32:26 +0200

    Linepiece-triangle intersection

--- src/algorithms/collision.rs
+++ src/algorithms/collision.rs
@@ -1,13 +1,13 @@
 
 extern crate ncollide3d;
 
-use na::{self, Real, Vector3};
+use na::{Real, Vector3};
 
 use types::*;
 use ids::*;
 use dynamic_mesh::DynamicMesh;
 
-use algorithms::collision::ncollide3d::query::{proximity, Proximity, Ray};
+use algorithms::collision::ncollide3d::query::{proximity, Proximity};
 use na::{Isometry3, Point3};
 
 type Triangle = ncollide3d::shape::Triangle<f32>;
@@ -18,7 +18,6 @@ pub fn find_intersection_point(mesh1: &DynamicMesh, face_id1: &FaceID, mesh2: &D
     let mut walker = mesh2.walker_from_halfedge(halfedge_id2);
     let p0 = Point::from_coordinates(*mesh2.position(&walker.vertex_id().unwrap()));
     let p1 = Point::from_coordinates(*mesh2.position(&walker.twin().vertex_id().unwrap()));
-    let ray = Ray::new(p0.clone(), p1 - p0);
 
     walker = mesh1.walker_from_face(face_id1);
     let a = Point::from_coordinates(*mesh1.position(&walker.vertex_id().unwrap()));
@@ -27,7 +26,7 @@ pub fn find_intersection_point(mesh1: &DynamicMesh, face_id1: &FaceID, mesh2: &D
     walker.next();
     let c = Point::from_coordinates(*mesh1.position(&walker.vertex_id().unwrap()));
 
-    triangle_ray_intersection(&a, &b, &c, &ray)
+    triangle_linepiece_intersection(&a, &b, &c, &p0, &p1)
 }
 
 pub fn is_intersecting(mesh1: &DynamicMesh, face_id1: &FaceID, mesh2: &DynamicMesh, face_id2: &FaceID) -> bool
@@ -50,34 +49,35 @@ fn face_id_to_triangle(mesh: &DynamicMesh, face_id: &FaceID) -> Triangle
     Triangle::new(p1, p2, p3)
 }
 
-fn triangle_ray_intersection<N: Real>(a: &Point3<N>, b: &Point3<N>, c: &Point3<N>, ray: &Ray<N>) -> Option<Vector3<N>>
+fn triangle_linepiece_intersection<N: Real>(a: &Point3<N>, b: &Point3<N>, c: &Point3<N>, p0: &Point3<N>, p1: &Point3<N>) -> Option<Vector3<N>>
 {
     let ab = *b - *a;
     let ac = *c - *a;
+    let dir = *p1 - *p0;
 
     // normal
     let n = ab.cross(&ac);
-    let d = ::na::dot(&n, &ray.dir);
+    let d = ::na::dot(&n, &dir);
 
-    // the normal and the ray direction are parallel
+    // the normal and the ray direction are orthogonal
     if d.is_zero() {
         return None;
     }
 
-    let ap = ray.origin - *a;
+    let ap = p0 - *a;
     let t = ::na::dot(&ap, &n);
 
     // the ray does not intersect the plane defined by the triangle
-    if (t < ::na::zero() && d < na::zero()) || (t > ::na::zero() && d > ::na::zero()) {
+    /*if (t < ::na::zero() && d < na::zero()) || (t > ::na::zero() && d > ::na::zero()) {
         return None;
-    }
+    }*/
 
     let d = d.abs();
 
     //
     // intersection: compute barycentric coordinates
     //
-    let e = -ray.dir.cross(&ap);
+    let e = -dir.cross(&ap);
 
     let toi;
     if t < ::na::zero() {
@@ -112,5 +112,5 @@ fn triangle_ray_intersection<N: Real>(a: &Point3<N>, b: &Point3<N>, c: &Point3<N
         toi = t * invd;
     }
 
-    Some(ray.origin.coords + ray.dir * toi)
+    Some(p0.coords + dir * toi)
 }
