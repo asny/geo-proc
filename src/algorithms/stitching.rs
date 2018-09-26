1512830b46569980796ba66462661136d70c361c
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-09-26 12:32:48 +0200

    Removed edge struct

--- src/algorithms/stitching.rs
+++ src/algorithms/stitching.rs
@@ -65,12 +65,12 @@ fn split_at_intersections(mesh1: &mut DynamicMesh, mesh2: &mut DynamicMesh) -> V
         let vertex_id1 = match id1 {
             PrimitiveID::Vertex(vertex_id) => { vertex_id },
             PrimitiveID::Edge(edge) => {
-                match find_type_to_split_edge(&edge_splits1, mesh1, Edge::new(edge.0, edge.1), &point) {
+                match find_type_to_split_edge(&edge_splits1, mesh1, edge, &point) {
                     PrimitiveID::Vertex(vertex_id) => { vertex_id },
-                    PrimitiveID::Edge(split_edge) => {
+                    PrimitiveID::Edge(ref split_edge) => {
                         let halfedge_id = connecting_edge(mesh1, &split_edge.0, &split_edge.1).unwrap();
                         let vertex_id = mesh1.split_edge(&halfedge_id, point);
-                        insert_edges(&mut edge_splits1, mesh1, Edge::new(edge.0, edge.1), &Edge::new(split_edge.0, split_edge.1), &vertex_id);
+                        insert_edges(&mut edge_splits1, mesh1, edge, split_edge, &vertex_id);
                         vertex_id
                     },
                     _ => {unreachable!()}
@@ -81,12 +81,12 @@ fn split_at_intersections(mesh1: &mut DynamicMesh, mesh2: &mut DynamicMesh) -> V
         let vertex_id2 = match id2 {
             PrimitiveID::Vertex(vertex_id) => { vertex_id },
             PrimitiveID::Edge(edge) => {
-                match find_type_to_split_edge(&edge_splits2, mesh2, Edge::new(edge.0, edge.1), &point) {
+                match find_type_to_split_edge(&edge_splits2, mesh2, edge, &point) {
                     PrimitiveID::Vertex(vertex_id) => { vertex_id },
-                    PrimitiveID::Edge(split_edge) => {
+                    PrimitiveID::Edge(ref split_edge) => {
                         let halfedge_id = connecting_edge(mesh2, &split_edge.0, &split_edge.1).unwrap();
                         let vertex_id = mesh2.split_edge(&halfedge_id, point);
-                        insert_edges(&mut edge_splits2, mesh2, Edge::new(edge.0, edge.1), &Edge::new(split_edge.0, split_edge.1), &vertex_id);
+                        insert_edges(&mut edge_splits2, mesh2, edge, split_edge, &vertex_id);
                         vertex_id
                     },
                     _ => {unreachable!()}
@@ -136,33 +136,33 @@ fn is_inside(mesh: &DynamicMesh, face_id: &FaceID, point: &Vec3) -> bool
     f.abs() <= 0.001
 }
 
-fn find_type_to_split_edge(edge_splits: &HashMap<Edge, HashSet<Edge>>, mesh: &DynamicMesh, edge: Edge, point: &Vec3) -> PrimitiveID
+fn find_type_to_split_edge(edge_splits: &HashMap<(VertexID, VertexID), HashSet<(VertexID, VertexID)>>, mesh: &DynamicMesh, edge: (VertexID, VertexID), point: &Vec3) -> PrimitiveID
 {
     if let Some(new_edges) = edge_splits.get(&edge)
     {
         for new_edge in new_edges
         {
-            let v1 = point - mesh.position(&new_edge.v0);
-            let v2 = point - mesh.position(&new_edge.v1);
+            let v1 = point - mesh.position(&new_edge.0);
+            let v2 = point - mesh.position(&new_edge.1);
             if v1.dot(&v2) < MARGIN
             {
                 if let Some(vertex_id) = find_close_vertex_on_edge(mesh, &edge, &point) {
                     return PrimitiveID::Vertex(vertex_id)
                 }
-                return PrimitiveID::Edge((new_edge.v0, new_edge.v1))
+                return PrimitiveID::Edge(new_edge.clone())
             }
         }
         panic!("ARGH")
     }
-    PrimitiveID::Edge((edge.v0, edge.v1))
+    PrimitiveID::Edge(edge)
 }
 
-fn insert_edges(edge_list: &mut HashMap<Edge, HashSet<Edge>>, mesh: &DynamicMesh, edge: Edge, split_edge: &Edge, vertex_id: &VertexID)
+fn insert_edges(edge_list: &mut HashMap<(VertexID, VertexID), HashSet<(VertexID, VertexID)>>, mesh: &DynamicMesh, edge: (VertexID, VertexID), split_edge: &(VertexID, VertexID), vertex_id: &VertexID)
 {
     if !edge_list.contains_key(&edge) { edge_list.insert(edge.clone(), HashSet::new()); }
     let list = edge_list.get_mut(&edge).unwrap();
-    list.insert(Edge::new(edge.v0, vertex_id.clone()));
-    list.insert(Edge::new(edge.v1, vertex_id.clone()));
+    list.insert((edge.0, vertex_id.clone()));
+    list.insert((edge.1, vertex_id.clone()));
 }
 
 fn insert_faces(face_list: &mut HashMap<FaceID, HashSet<FaceID>>, mesh: &DynamicMesh, face_id: FaceID, vertex_id: &VertexID)
@@ -174,20 +174,6 @@ fn insert_faces(face_list: &mut HashMap<FaceID, HashSet<FaceID>>, mesh: &Dynamic
     list.insert(iter.next().unwrap().face_id().unwrap());
     list.insert(iter.next().unwrap().face_id().unwrap());
     list.insert(iter.next().unwrap().face_id().unwrap());
-}
-
-#[derive(Clone, Debug, Hash, Eq, PartialEq)]
-struct Edge
-{
-    pub v0: VertexID,
-    pub v1: VertexID
-}
-
-impl Edge {
-    fn new(v0: VertexID, v1: VertexID) -> Edge
-    {
-        if v0 < v1 {Edge{v0, v1}} else {Edge{v0: v1, v1: v0}}
-    }
 }
 
 fn find_intersections(mesh1: &DynamicMesh, mesh2: &DynamicMesh) -> HashMap<(PrimitiveID, PrimitiveID), Vec3>
@@ -206,7 +192,7 @@ fn find_intersections_between_edge_face(mesh1: &DynamicMesh, edges1: &Vec<(Verte
         {
             if let Some(point) = find_intersection_point(mesh2, &face_id2, mesh1,edge1)
             {
-                let id1 = match find_close_vertex_on_edge(mesh1,&Edge::new(edge1.0, edge1.1), &point) {
+                let id1 = match find_close_vertex_on_edge(mesh1,&edge1, &point) {
                     Some(vertex_id1) => PrimitiveID::Vertex(vertex_id1),
                     None => PrimitiveID::Edge(*edge1)
                 };
@@ -225,7 +211,7 @@ fn find_intersections_between_edge_face(mesh1: &DynamicMesh, edges1: &Vec<(Verte
         {
             if let Some(point) = find_intersection_point(mesh1, &face_id1, mesh2, edge2)
             {
-                let id2 = match find_close_vertex_on_edge(mesh2,&Edge::new(edge2.0, edge2.1), &point) {
+                let id2 = match find_close_vertex_on_edge(mesh2,&edge2, &point) {
                     Some(vertex_id2) => PrimitiveID::Vertex(vertex_id2),
                     None => PrimitiveID::Edge(*edge2)
                 };
@@ -257,14 +243,14 @@ fn find_close_type(mesh: &DynamicMesh, face_id: FaceID, point: &Vec3) -> Primiti
     }
     else if let Some(edge) = find_close_edge(mesh, &face_id, &point)
     {
-        return PrimitiveID::Edge((edge.v0, edge.v1))
+        return PrimitiveID::Edge(edge)
     }
     PrimitiveID::Face(face_id)
 }
 
 const MARGIN: f32 = 0.01;
 
-fn find_close_edge(mesh: &DynamicMesh, face_id: &FaceID, point: &Vec3) -> Option<Edge>
+fn find_close_edge(mesh: &DynamicMesh, face_id: &FaceID, point: &Vec3) -> Option<(VertexID, VertexID)>
 {
     let mut walker = mesh.walker_from_face(face_id);
     let vertex_id1 = walker.vertex_id().unwrap();
@@ -272,17 +258,17 @@ fn find_close_edge(mesh: &DynamicMesh, face_id: &FaceID, point: &Vec3) -> Option
     let vertex_id2 = walker.vertex_id().unwrap();
 
     if point_line_segment_distance(point, mesh.position(&vertex_id1), mesh.position(&vertex_id2)) < MARGIN {
-        return Some(Edge::new(vertex_id1, vertex_id2))
+        return Some((vertex_id1, vertex_id2))
     }
 
     walker.next();
     let vertex_id3 = walker.vertex_id().unwrap();
 
     if point_line_segment_distance(point, mesh.position(&vertex_id2), mesh.position(&vertex_id3)) < MARGIN {
-        return Some(Edge::new(vertex_id2, vertex_id3))
+        return Some((vertex_id2, vertex_id3))
     }
     if point_line_segment_distance(point, mesh.position(&vertex_id3), mesh.position(&vertex_id1)) < MARGIN {
-        return Some(Edge::new(vertex_id3, vertex_id1))
+        return Some((vertex_id3, vertex_id1))
     }
     None
 }
@@ -298,15 +284,15 @@ fn find_close_vertex_on_face(mesh: &DynamicMesh, face_id: &FaceID, point: &Vec3)
     None
 }
 
-fn find_close_vertex_on_edge(mesh: &DynamicMesh, edge: &Edge, point: &Vec3) -> Option<VertexID>
+fn find_close_vertex_on_edge(mesh: &DynamicMesh, edge: &(VertexID, VertexID), point: &Vec3) -> Option<VertexID>
 {
-    if(point - mesh.position(&edge.v0)).norm() < MARGIN
+    if(point - mesh.position(&edge.0)).norm() < MARGIN
     {
-        return Some(edge.v0)
+        return Some(edge.0)
     }
-    if (point - mesh.position(&edge.v1)).norm() < MARGIN
+    if (point - mesh.position(&edge.1)).norm() < MARGIN
     {
-        return Some(edge.v1)
+        return Some(edge.1)
     }
     None
 }
