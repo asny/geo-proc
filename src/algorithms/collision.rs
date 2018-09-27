c3270569edc8a07da2dca66d6da6dacc26b3aa06
Asger <asger.nyman@alexandra.dk>
2018-09-27 12:10:09 +0200

    Intersection when at least one vertex lies in the same plane as the face

--- src/algorithms/collision.rs
+++ src/algorithms/collision.rs
@@ -42,24 +42,25 @@ pub fn find_face_edge_intersections(mesh1: &DynamicMesh, face_id: &FaceID, mesh2
     let d1 = n.dot(&ap1);
 
     if d0.abs() < MARGIN && d1.abs() < MARGIN { // p0 and p1 lies in the same plane as the face
-        // TODO: p0 and p1 lies in the same plane as the face
-        if let Some(vertex_id) = is_close_to_vertex(p0, a, b, c, &face_vertices) {
-            intersections.push(Intersection {id1: PrimitiveID::Vertex(edge.0), id2: PrimitiveID::Vertex(vertex_id), point: p0.clone()});
+        if let Some(id1) = find_face_intersection(p0, a, b, c, face_vertices.0, face_vertices.1, face_vertices.2, face_id.clone() ) {
+            let id2 = PrimitiveID::Vertex(edge.0);
+            intersections.push(Intersection{id1, id2, point: *p0});
         }
-        if let Some(vertex_id) = is_close_to_vertex(p1, a, b, c, &face_vertices) {
-            intersections.push(Intersection {id1: PrimitiveID::Vertex(edge.1), id2: PrimitiveID::Vertex(vertex_id), point: p1.clone()});
+        if let Some(id1) = find_face_intersection(p1, a, b, c, face_vertices.0, face_vertices.1, face_vertices.2, face_id.clone() ) {
+            let id2 = PrimitiveID::Vertex(edge.1);
+            intersections.push(Intersection{id1, id2, point: *p1});
         }
     }
     else if d0.abs() < MARGIN { // p0 lies in the same plane as the face
-        // TODO: p0 lies in the same plane as the face
-        if let Some(vertex_id) = is_close_to_vertex(p0, a, b, c, &face_vertices) {
-            intersections.push(Intersection {id1: PrimitiveID::Vertex(edge.0), id2: PrimitiveID::Vertex(vertex_id), point: p0.clone()});
+        if let Some(id1) = find_face_intersection(p0, a, b, c, face_vertices.0, face_vertices.1, face_vertices.2, face_id.clone() ) {
+            let id2 = PrimitiveID::Vertex(edge.0);
+            intersections.push(Intersection{id1, id2, point: *p0});
         }
     }
     else if d1.abs() < MARGIN { // p1 lies in the same plane as the face
-        // TODO: p1 lies in the same plane as the face
-        if let Some(vertex_id) = is_close_to_vertex(p1, a, b, c, &face_vertices) {
-            intersections.push(Intersection {id1: PrimitiveID::Vertex(edge.1), id2: PrimitiveID::Vertex(vertex_id), point: p1.clone()});
+        if let Some(id1) = find_face_intersection(p1, a, b, c, face_vertices.0, face_vertices.1, face_vertices.2, face_id.clone() ) {
+            let id2 = PrimitiveID::Vertex(edge.1);
+            intersections.push(Intersection{id1, id2, point: *p1});
         }
     }
     else if d0.signum() != d1.signum() // The edge intersects the plane spanned by the face
