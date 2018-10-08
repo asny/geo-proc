6b3098f92c9ff0624c66d8f4f5166cfa5e9d516a
Asger <asger.nyman@alexandra.dk>
2018-10-08 15:34:32 +0200

    Merge tests

--- src/dynamic_mesh/splitting_and_merging.rs
+++ src/dynamic_mesh/splitting_and_merging.rs
@@ -95,22 +95,25 @@ impl DynamicMesh
             let vertex_id2 = get_or_create_vertex(self, vertex_ids.2);
 
             if stitches.contains_key(&vertex_ids.0) && stitches.contains_key(&vertex_ids.1)
-                && other.connecting_edge(&vertex_ids.0, &vertex_ids.1).is_some()
             {
-                let halfedge_id = self.connecting_edge(&vertex_id0, &vertex_id1).unwrap();
-                stitch_edge(self, halfedge_id);
+                if let Some(halfedge_id) = self.connecting_edge(&vertex_ids.0, &vertex_ids.1)
+                {
+                    stitch_edge(self, halfedge_id);
+                }
             }
             if stitches.contains_key(&vertex_ids.1) && stitches.contains_key(&vertex_ids.2)
-                && other.connecting_edge(&vertex_ids.1, &vertex_ids.2).is_some()
             {
-                let halfedge_id = self.connecting_edge(&vertex_id1, &vertex_id2).unwrap();
-                stitch_edge(self, halfedge_id);
+                if let Some(halfedge_id) = self.connecting_edge(&vertex_ids.1, &vertex_ids.2)
+                {
+                    stitch_edge(self, halfedge_id);
+                }
             }
             if stitches.contains_key(&vertex_ids.2) && stitches.contains_key(&vertex_ids.0)
-                && other.connecting_edge(&vertex_ids.2, &vertex_ids.0).is_some()
             {
-                let halfedge_id = self.connecting_edge(&vertex_id2, &vertex_id0).unwrap();
-                stitch_edge(self, halfedge_id);
+                if let Some(halfedge_id) = self.connecting_edge(&vertex_ids.2, &vertex_ids.0)
+                {
+                    stitch_edge(self, halfedge_id);
+                }
             }
 
             self.connectivity_info.create_face(&vertex_id0, &vertex_id1, &vertex_id2);
@@ -126,4 +129,71 @@ pub fn merge(mesh1: &DynamicMesh, mesh2: &DynamicMesh, stitches: &HashMap<Vertex
     let mut mesh = mesh1.clone();
     mesh.merge_with(mesh2, stitches);
     mesh
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_face_face_merging_at_edge()
+    {
+        let indices1: Vec<u32> = vec![0, 1, 2];
+        let positions1: Vec<f32> = vec![-2.0, 0.0, -2.0, -2.0, 0.0, 2.0, 2.0, 0.0, 0.0];
+        let mut mesh1 = DynamicMesh::create(indices1, positions1, None);
+
+        let indices2: Vec<u32> = vec![0, 1, 2];
+        let positions2: Vec<f32> = vec![-2.0, 0.0, 2.0, -2.0, 0.0, -2.0, -2.0, 0.5, 0.0];
+        let mut mesh2 = DynamicMesh::create(indices2, positions2, None);
+
+        let mut mapping = HashMap::new();
+        for vertex_id1 in mesh1.vertex_iterator() {
+            for vertex_id2 in mesh2.vertex_iterator() {
+                if (*mesh1.position(&vertex_id1) - *mesh2.position(&vertex_id2)).norm() < 0.001 {
+                    mapping.insert(vertex_id2, vertex_id1);
+                }
+            }
+        }
+
+        let stitched = merge(&mesh1, &mesh2, &mapping);
+
+        mesh1.test_is_valid().unwrap();
+        mesh2.test_is_valid().unwrap();
+
+        assert_eq!(stitched.no_faces(), 2);
+        assert_eq!(stitched.no_vertices(), 4);
+        stitched.test_is_valid().unwrap();
+    }
+
+    #[test]
+    fn test_face_face_merging_at_edge_when_orientation_is_opposite()
+    {
+        let indices1: Vec<u32> = vec![0, 1, 2];
+        let positions1: Vec<f32> = vec![-2.0, 0.0, -2.0, -2.0, 0.0, 2.0, 2.0, 0.0, 0.0];
+        let mut mesh1 = DynamicMesh::create(indices1, positions1, None);
+
+        let indices2: Vec<u32> = vec![0, 1, 2];
+        let positions2: Vec<f32> = vec![-2.0, 0.0, 2.0, -2.0, 0.5, 0.0, -2.0, 0.0, -2.0];
+        let mut mesh2 = DynamicMesh::create(indices2, positions2, None);
+
+        let mut mapping = HashMap::new();
+        for vertex_id1 in mesh1.vertex_iterator() {
+            for vertex_id2 in mesh2.vertex_iterator() {
+                if (*mesh1.position(&vertex_id1) - *mesh2.position(&vertex_id2)).norm() < 0.001 {
+                    mapping.insert(vertex_id2, vertex_id1);
+                }
+            }
+        }
+
+        let stitched = merge(&mesh1, &mesh2, &mapping);
+
+        mesh1.test_is_valid().unwrap();
+        mesh2.test_is_valid().unwrap();
+
+        assert_eq!(stitched.no_faces(), 2);
+        assert_eq!(stitched.no_vertices(), 4);
+        stitched.test_is_valid().unwrap();
+    }
 }
