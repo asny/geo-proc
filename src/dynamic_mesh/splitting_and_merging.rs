522b5991fe5db9584710441ea4276d561ed886f6
Asger <asger.nyman@alexandra.dk>
2018-10-09 10:13:37 +0200

    Fix create_sub_mesh + test_create_sub_mesh

--- src/dynamic_mesh/splitting_and_merging.rs
+++ src/dynamic_mesh/splitting_and_merging.rs
@@ -11,18 +11,32 @@ impl DynamicMesh
         let info = connectivity_info::ConnectivityInfo::new(faces.len(), faces.len());
         for face_id in faces {
             let face = self.connectivity_info.face(face_id).unwrap();
-            for walker in self.face_halfedge_iterator(face_id) {
+            for mut walker in self.face_halfedge_iterator(face_id) {
                 let halfedge_id = walker.halfedge_id().unwrap();
                 let halfedge = self.connectivity_info.halfedge(&halfedge_id).unwrap();
                 info.add_halfedge(halfedge_id, halfedge);
 
-                let twin_id = walker.twin_id().unwrap();
-                let twin = self.connectivity_info.halfedge(&twin_id).unwrap();
-                info.add_halfedge(twin_id, twin);
-
                 let vertex_id = walker.vertex_id().unwrap();
                 let vertex = self.connectivity_info.vertex(&vertex_id).unwrap();
                 info.add_vertex(vertex_id, vertex);
+                info.set_vertex_halfedge(&vertex_id, walker.next_id().unwrap());
+
+                walker.twin();
+                if walker.face_id().is_none()
+                {
+                    let twin_id = walker.halfedge_id().unwrap();
+                    let mut twin = self.connectivity_info.halfedge(&twin_id).unwrap();
+                    info.add_halfedge(twin_id, twin);
+
+                }
+                else if !faces.contains(&walker.face_id().unwrap())
+                {
+                    let twin_id = walker.halfedge_id().unwrap();
+                    let mut twin = self.connectivity_info.halfedge(&twin_id).unwrap();
+                    twin.face = None;
+                    twin.next = None;
+                    info.add_halfedge(twin_id, twin);
+                }
             }
 
             info.add_face(face_id.clone(), face);
@@ -201,6 +215,25 @@ mod tests {
         assert_eq!(stitched.no_faces(), 2);
         assert_eq!(stitched.no_vertices(), 4);
         stitched.test_is_valid().unwrap();
+    }
+
+    #[test]
+    fn test_create_sub_mesh()
+    {
+        let indices: Vec<u32> = vec![0, 1, 2,  2, 1, 3,  3, 1, 4,  3, 4, 5];
+        let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0, 0.5,  1.0, 0.0, 1.5,  0.0, 0.0, 2.0,  1.0, 0.0, 2.5];
+        let mesh = DynamicMesh::create(indices, positions, None);
+
+        let mut faces = HashSet::new();
+        for face_id in mesh.face_iterator() {
+            faces.insert(face_id);
+            break;
+        }
+
+        let sub_mesh = mesh.create_sub_mesh(&faces);
+
+        mesh.test_is_valid().unwrap();
+        sub_mesh.test_is_valid().unwrap();
     }
 
     #[test]
