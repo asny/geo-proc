3ab083ca5d2eb208a9c41e25d425714373a19895
Asger <asger.nyman@alexandra.dk>
2018-10-15 15:47:01 +0200

    create_icosahedron

--- src/models.rs
+++ src/models.rs
@@ -2,10 +2,29 @@
 use mesh::*;
 use std;
 
-pub fn create_cylinder() -> Result<StaticMesh, Error>
+pub fn create_icosahedron() -> Result<StaticMesh, Error>
 {
-    let x_subdivisions = 10;
-    let angle_subdivisions = 10;
+    let x = 0.525731112119133606;
+    let z = 0.850650808352039932;
+
+    let positions = vec!(
+       -x, 0.0, z, x, 0.0, z, -x, 0.0, -z, x, 0.0, -z,
+       0.0, z, x, 0.0, z, -x, 0.0, -z, x, 0.0, -z, -x,
+       z, x, 0.0, -z, x, 0.0, z, -x, 0.0, -z, -x, 0.0
+    );
+    let indices = vec!(
+       0,4,1, 0,9,4, 9,5,4, 4,5,8, 4,8,1,
+       8,10,1, 8,3,10, 5,3,8, 5,2,3, 2,7,3,
+       7,10,3, 7,6,10, 7,11,6, 11,0,6, 0,1,6,
+       6,1,10, 9,0,11, 9,11,2, 9,2,5, 7,2,11
+    );
+
+    let mesh = StaticMesh::create(indices, att!["position" => (positions, 3)])?;
+    Ok(mesh)
+}
+
+pub fn create_cylinder(x_subdivisions: usize, angle_subdivisions: usize) -> Result<StaticMesh, Error>
+{
     let mut positions = Vec::new();
     let mut indices = Vec::new();
     for i in 0..x_subdivisions+1 {
@@ -18,15 +37,15 @@ pub fn create_cylinder() -> Result<StaticMesh, Error>
             positions.push(angle.sin());
         }
     }
-    for i in 0..x_subdivisions {
-        for j in 0..angle_subdivisions {
-            indices.push(i * angle_subdivisions + j);
-            indices.push(i * angle_subdivisions + (j+1)%angle_subdivisions);
-            indices.push((i+1) * angle_subdivisions + (j+1)%angle_subdivisions);
+    for i in 0..x_subdivisions as u32 {
+        for j in 0..angle_subdivisions as u32 {
+            indices.push(i * angle_subdivisions as u32 + j);
+            indices.push(i * angle_subdivisions as u32 + (j+1)%angle_subdivisions as u32);
+            indices.push((i+1) * angle_subdivisions as u32 + (j+1)%angle_subdivisions as u32);
 
-            indices.push(i * angle_subdivisions + j);
-            indices.push((i+1) * angle_subdivisions + (j+1)%angle_subdivisions);
-            indices.push((i+1) * angle_subdivisions + j);
+            indices.push(i * angle_subdivisions as u32 + j);
+            indices.push((i+1) * angle_subdivisions as u32 + (j+1)%angle_subdivisions as u32);
+            indices.push((i+1) * angle_subdivisions as u32 + j);
         }
     }
 
