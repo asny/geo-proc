d1f3a032bcd2630cfc9d9667f52a7502a9fb32c0
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-10-17 12:59:28 +0200

    Functions for flip_edges

--- src/dynamic_mesh/quality.rs
+++ src/dynamic_mesh/quality.rs
@@ -2,7 +2,25 @@ use dynamic_mesh::*;
 
 impl DynamicMesh
 {
-    pub fn flip_edges(&self, vertex_id1: &VertexID, vertex_id2: &VertexID)
+    pub fn flip_edges(&mut self)
     {
+        let edges = self.halfedge_iterator();
+        for halfedge_id in edges
+        {
+            if !self.on_boundary(&halfedge_id) && self.flatness(&halfedge_id) < 0.1 && self.flip_will_improve_quality(&halfedge_id)
+            {
+                self.flip_edge(&halfedge_id).unwrap();
+            }
+        }
+    }
+
+    fn flatness(&self, haledge_id: &HalfEdgeID) -> f32
+    {
+        0.0
+    }
+
+    fn flip_will_improve_quality(&self, haledge_id: &HalfEdgeID) -> bool
+    {
+        true
     }
 }
