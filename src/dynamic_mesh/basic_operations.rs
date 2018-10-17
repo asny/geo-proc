7a6a0bb128239e338f5dc22732c13618dbed5e69
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-10-17 12:52:14 +0200

    Test of flip_edge

--- src/dynamic_mesh/basic_operations.rs
+++ src/dynamic_mesh/basic_operations.rs
@@ -11,14 +11,14 @@ impl DynamicMesh
     pub fn flip_edge(&mut self, halfedge_id: &HalfEdgeID) -> Result<(), Error>
     {
         let mut walker = self.walker_from_halfedge(halfedge_id);
-        let face_id = walker.face_id().unwrap_or(return Err(Error::CannotFlipEdgeOnBoundary {message: format!("Trying to flip edge on boundary")}));
+        let face_id = walker.face_id().ok_or(Error::CannotFlipEdgeOnBoundary {message: format!("Trying to flip edge on boundary")})?;
         let next_id = walker.next_id().unwrap();
         let previous_id = walker.previous_id().unwrap();
         let v0 = walker.vertex_id().unwrap();
 
         walker.twin();
         let twin_id = walker.halfedge_id().unwrap();
-        let twin_face_id = walker.face_id().unwrap_or(return Err(Error::CannotFlipEdgeOnBoundary {message: format!("Trying to flip edge on boundary")}));
+        let twin_face_id = walker.face_id().ok_or(Error::CannotFlipEdgeOnBoundary {message: format!("Trying to flip edge on boundary")})?;
         let twin_next_id = walker.next_id().unwrap();
         let twin_previous_id = walker.previous_id().unwrap();
         let v1 = walker.vertex_id().unwrap();
@@ -46,5 +46,28 @@ impl DynamicMesh
         self.connectivity_info.set_halfedge_face(&twin_next_id, face_id);
 
         Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_flip_edge()
+    {
+        let mut mesh = ::models::create_plane().unwrap().to_dynamic();
+        for halfedge_id in mesh.halfedge_iterator() {
+            if !mesh.on_boundary(&halfedge_id) {
+                let (v0, v1) = mesh.edge_vertices(&halfedge_id);
+                mesh.flip_edge(&halfedge_id).unwrap();
+                let (v2, v3) = mesh.edge_vertices(&halfedge_id);
+                assert_ne!(v0, v2);
+                assert_ne!(v1, v2);
+                assert_ne!(v0, v3);
+                assert_ne!(v1, v3);
+
+                mesh.test_is_valid().unwrap();
+            }
+        }
     }
 }
