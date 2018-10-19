26d7a207f981118b59f0878c3b890ea3c9198ca8
Asger <asger.nyman@alexandra.dk>
2018-10-19 13:48:08 +0200

    Fixed flip_edge error when resulting edge is connecting the same vertices as an existing edge

--- src/dynamic_mesh/basic_operations.rs
+++ src/dynamic_mesh/basic_operations.rs
@@ -3,7 +3,7 @@ use dynamic_mesh::*;
 
 #[derive(Debug)]
 pub enum Error {
-    CannotFlipEdgeOnBoundary {message: String}
+    FailedToFlipEdge {message: String},
 }
 
 impl DynamicMesh
@@ -11,20 +11,23 @@ impl DynamicMesh
     pub fn flip_edge(&mut self, halfedge_id: &HalfEdgeID) -> Result<(), Error>
     {
         let mut walker = self.walker_from_halfedge(halfedge_id);
-        let face_id = walker.face_id().ok_or(Error::CannotFlipEdgeOnBoundary {message: format!("Trying to flip edge on boundary")})?;
+        let face_id = walker.face_id().ok_or(Error::FailedToFlipEdge {message: format!("Trying to flip edge on boundary")})?;
         let next_id = walker.next_id().unwrap();
         let previous_id = walker.previous_id().unwrap();
         let v0 = walker.vertex_id().unwrap();
+        walker.next();
+        let v3 = walker.vertex_id().unwrap();
+        walker.previous();
 
         walker.twin();
         let twin_id = walker.halfedge_id().unwrap();
-        let twin_face_id = walker.face_id().ok_or(Error::CannotFlipEdgeOnBoundary {message: format!("Trying to flip edge on boundary")})?;
+        let twin_face_id = walker.face_id().ok_or(Error::FailedToFlipEdge {message: format!("Trying to flip edge on boundary")})?;
         let twin_next_id = walker.next_id().unwrap();
         let twin_previous_id = walker.previous_id().unwrap();
         let v1 = walker.vertex_id().unwrap();
-
         let v2 = walker.next().vertex_id().unwrap();
-        let v3 = walker.previous().twin().next().vertex_id().unwrap();
+
+        if self.connecting_edge(&v2, &v3).is_some() { return Err(Error::FailedToFlipEdge {message: format!("Trying to flip edge which will connect two vertices that are already connected by another edge")}) }
 
         self.connectivity_info.set_face_halfedge(&face_id, previous_id);
         self.connectivity_info.set_face_halfedge(&twin_face_id, twin_previous_id);
@@ -56,18 +59,59 @@ mod tests {
     fn test_flip_edge()
     {
         let mut mesh = ::models::create_plane().unwrap().to_dynamic();
+        let no_edges = mesh.no_halfedges();
         for halfedge_id in mesh.halfedge_iterator() {
-            if !mesh.on_boundary(&halfedge_id) {
-                let (v0, v1) = mesh.edge_vertices(&halfedge_id);
-                mesh.flip_edge(&halfedge_id).unwrap();
+            let (v0, v1) = mesh.edge_vertices(&halfedge_id);
+
+            if mesh.flip_edge(&halfedge_id).is_ok()
+            {
+                mesh.test_is_valid().unwrap();
+
                 let (v2, v3) = mesh.edge_vertices(&halfedge_id);
                 assert_ne!(v0, v2);
                 assert_ne!(v1, v2);
                 assert_ne!(v0, v3);
                 assert_ne!(v1, v3);
 
-                mesh.test_is_valid().unwrap();
+                assert!(mesh.connecting_edge(&v0, &v1).is_none());
+                assert!(mesh.connecting_edge(&v2, &v3).is_some());
+
+                let edge = mesh.connecting_edge(&v2, &v3).unwrap();
+                let twin = mesh.walker_from_halfedge(&edge).twin_id().unwrap();
+                assert!(edge == halfedge_id || twin == halfedge_id,
+                        format!("Flipped edge {} or flipped edge twin {} should be equal to before flipped edge id {}", edge, twin, halfedge_id));
             }
         }
+        assert_eq!(no_edges, mesh.no_halfedges());
+    }
+
+    #[test]
+    fn test_flip_multiple_edges()
+    {
+        let mut mesh = ::models::create_icosahedron().unwrap().to_dynamic();
+        let no_edges = mesh.no_halfedges();
+        for halfedge_id in mesh.halfedge_iterator() {
+            let (v0, v1) = mesh.edge_vertices(&halfedge_id);
+
+            if mesh.flip_edge(&halfedge_id).is_ok()
+            {
+                mesh.test_is_valid().unwrap();
+
+                let (v2, v3) = mesh.edge_vertices(&halfedge_id);
+                assert_ne!(v0, v2);
+                assert_ne!(v1, v2);
+                assert_ne!(v0, v3);
+                assert_ne!(v1, v3);
+
+                assert!(mesh.connecting_edge(&v0, &v1).is_none());
+                assert!(mesh.connecting_edge(&v2, &v3).is_some());
+
+                let edge = mesh.connecting_edge(&v2, &v3).unwrap();
+                let twin = mesh.walker_from_halfedge(&edge).twin_id().unwrap();
+                assert!(edge == halfedge_id || twin == halfedge_id,
+                        format!("Flipped edge {} or flipped edge twin {} should be equal to before flipped edge id {}", edge, twin, halfedge_id));
+            }
+        }
+        assert_eq!(no_edges, mesh.no_halfedges());
     }
 }
