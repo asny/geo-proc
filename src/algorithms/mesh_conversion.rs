92bcb86f728b6be9726a4f91a4aa8d51ad131aa7
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-10-22 21:58:45 +0200

    Clean-up in mesh_conversion

--- src/algorithms/mesh_conversion.rs
+++ src/algorithms/mesh_conversion.rs
@@ -1,14 +1,30 @@
 use mesh::*;
 
+impl StaticMesh
+{
+    pub fn to_dynamic(&self) -> DynamicMesh
+    {
+        let indices = self.indices().clone();
+        let positions = self.attribute("position").unwrap().data;
+        let normals = self.attribute("normal").map(|att| att.data);
+
+        DynamicMesh::create(indices, positions, normals)
+    }
+}
+
 impl DynamicMesh
 {
     pub fn to_static(&self) -> StaticMesh
     {
         let indices = indices(self);
-        let positions = get_attribute(self, "position").unwrap().data;
 
-        if let Some(normals) = get_attribute(self, "normal") {
-            StaticMesh::create(indices, att!["position" => (positions, 3), "normal" => (normals.data, 3)]).unwrap()
+        let mut positions = Vec::with_capacity(self.no_vertices() * 3);
+        for v3 in self.vertex_iterator().map(|ref vertex_id| self.position(vertex_id)) {
+            positions.push(v3.x); positions.push(v3.y); positions.push(v3.z);
+        }
+
+        if let Some(normals) = normals(self) {
+            StaticMesh::create(indices, att!["position" => (positions, 3), "normal" => (normals, 3)]).unwrap()
         }
         else {
             StaticMesh::create(indices, att!["position" => (positions, 3)]).unwrap()
@@ -31,39 +47,15 @@ fn indices(mesh: &DynamicMesh) -> Vec<u32>
     indices
 }
 
-fn get_attribute(mesh: &DynamicMesh, name: &str) -> Option<Attribute>
+fn normals(mesh: &DynamicMesh) -> Option<Vec<f32>>
 {
-    match name {
-        "position" => {
-            let mut pos = Vec::with_capacity(mesh.no_vertices() * 3);
-            for v3 in mesh.vertex_iterator().map(|ref vertex_id| mesh.position(vertex_id)) {
-                pos.push(v3.x); pos.push(v3.y); pos.push(v3.z);
-            }
-            Some(Attribute::new("position", 3, pos))
-        },
-        "normal" => {
-            let mut nor = Vec::with_capacity(mesh.no_vertices() * 3);
-            for vertex_id in mesh.vertex_iterator() {
-                if let Some(normal) = mesh.normal(&vertex_id)
-                {
-                    nor.push(normal.x); nor.push(normal.y); nor.push(normal.z);
-                }
-                else { return None; }
-            }
-            Some(Attribute::new("normal", 3, nor))
-        },
-        _ => None
+    let mut nor = Vec::with_capacity(mesh.no_vertices() * 3);
+    for vertex_id in mesh.vertex_iterator() {
+        if let Some(normal) = mesh.normal(&vertex_id)
+        {
+            nor.push(normal.x); nor.push(normal.y); nor.push(normal.z);
+        }
+        else { return None; }
     }
-}
-
-impl StaticMesh
-{
-    pub fn to_dynamic(&self) -> DynamicMesh
-    {
-        let indices = self.indices().clone();
-        let positions = self.attribute("position").unwrap().data;
-        let normals = self.attribute("normal").map(|att| att.data);
-
-        DynamicMesh::create(indices, positions, normals)
-    }
+    Some(nor)
 }
