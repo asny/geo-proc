29e82307fd36675cadb1ffabfc5b508de279a464
Asger <asger.nyman@alexandra.dk>
2018-11-28 16:01:12 +0100

    Fix test_splitting

--- src/dynamic_mesh/splitting_and_merging.rs
+++ src/dynamic_mesh/splitting_and_merging.rs
@@ -85,8 +85,8 @@ impl DynamicMesh
             connected_component_with_limit(self, &face_id, &|halfedge_id| is_at_split(self, &halfedge_id))
         } else { HashSet::new() };
 
-            return Err(Error::SplittingEdgesDidNotFormAClosesCurve {message: format!("It was not possible to split a mesh in two parts, the splitting edges did not form a closed curve.")})
         if self.no_faces() != cc1.len() + cc2.len() {
+            return Err(Error::SplittingEdgesDidNotFormAClosesCurve {message: format!("It was not possible to split a mesh in two parts, the splitting edges and boundary edges did not form a closed curve.")})
         }
 
         let sub_mesh1 = self.create_sub_mesh(&cc1);
@@ -261,19 +261,17 @@ mod tests {
         let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  0.0, 0.0, 1.0,  1.0, 0.0, 0.5,  1.0, 0.0, 1.5,  0.0, 0.0, 2.0,  1.0, 0.0, 2.5];
         let mesh = DynamicMesh::create(indices, positions, None);
 
-        let mut id = None;
-        for halfedge_id in mesh.halfedge_iterator() {
-            let mut walker = mesh.walker_from_halfedge(&halfedge_id);
-            if walker.face_id().is_some() && walker.twin().face_id().is_some()
-            {
-                id = Some(halfedge_id);
-            }
-        }
-
-        let (m1, m2) = mesh.split(&|mesh, he_id| {Some(*he_id) == id}).unwrap();
+        let (m1, m2) = mesh.split(&|mesh,
+            he_id| {
+                let (p0, p1) = mesh.edge_positions(he_id);
+                p0.z > 0.75 && p0.z < 1.75 && p1.z > 0.75 && p1.z < 1.75
+            }).unwrap();
         
         test_is_valid(&mesh).unwrap();
         test_is_valid(&m1).unwrap();
         test_is_valid(&m2).unwrap();
+
+        assert_eq!(m1.no_faces(), 2);
+        assert_eq!(m2.no_faces(), 2);
     }
 }
