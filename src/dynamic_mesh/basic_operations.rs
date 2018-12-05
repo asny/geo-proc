3992eb2a8a41a654bebf03cef48c57f69f60be43
Asger <asger.nyman@alexandra.dk>
2018-12-05 12:25:19 +0100

    test_merge_vertices_at_one_face

--- src/dynamic_mesh/basic_operations.rs
+++ src/dynamic_mesh/basic_operations.rs
@@ -682,7 +682,6 @@ mod tests {
             {
                 if vertex_id1.is_none() { vertex_id1 = Some(vertex_id); }
                 else {
-                    println!("Merging: {} and {}", vertex_id1.unwrap(), vertex_id);
                     mesh.merge_vertices(&vertex_id1.unwrap(), &vertex_id).unwrap();
                     break;
                 }
@@ -692,6 +691,35 @@ mod tests {
         assert_eq!(4, mesh.no_vertices());
         assert_eq!(10, mesh.no_halfedges());
         assert_eq!(2, mesh.no_faces());
+        test_is_valid(&mesh).unwrap();
+    }
+
+    #[test]
+    fn test_merge_vertices_at_one_face()
+    {
+        let indices: Vec<u32> = vec![0, 1, 2,  1, 2, 3  ];
+        let positions: Vec<f32> = vec![0.0, 0.0, 0.0,  1.0, 0.0, -0.5,  -1.0, 0.0, -0.5,
+                                       0.0, 0.0, 0.0];
+
+        let mut mesh = DynamicMesh::new_with_connectivity(indices, positions, None);
+        test_is_valid(&mesh).unwrap();
+
+        let mut vertex_id1 = None;
+        for vertex_id in mesh.vertex_iterator() {
+            if *mesh.position(&vertex_id) == vec3(0.0, 0.0, 0.0)
+            {
+                if vertex_id1.is_none() { vertex_id1 = Some(vertex_id); }
+                else {
+                    println!("Merging: {} and {}", vertex_id1.unwrap(), vertex_id);
+                    mesh.merge_vertices(&vertex_id1.unwrap(), &vertex_id).unwrap();
+                    break;
+                }
+            }
+        }
+
+        assert_eq!(3, mesh.no_vertices());
+        assert_eq!(6, mesh.no_halfedges());
+        assert_eq!(1, mesh.no_faces());
         test_is_valid(&mesh).unwrap();
     }
 }
