f1f92dfca872b0dd66c575a3da4d94b2a524a3be
Asger Nyman Christiansen <asgernyman@gmail.com>
2018-12-29 21:11:18 +0100

    Moved math functionality into mod

--- src/dynamic_mesh/mod.rs
+++ src/dynamic_mesh/mod.rs
@@ -1,19 +1,20 @@
 
-use cgmath::{Vector2, Vector3};
-pub use cgmath::prelude::*;
+pub mod math {
+    use cgmath::{Vector2, Vector3};
+    pub use cgmath::prelude::*;
 
-pub type Vec2 = Vector2<f32>;
-pub type Vec3 = Vector3<f32>;
+    pub type Vec2 = Vector2<f32>;
+    pub type Vec3 = Vector3<f32>;
 
-pub fn vec2(x: f32, y: f32) -> Vec2
-{
-    Vector2::new(x, y)
+    pub fn vec2(x: f32, y: f32) -> Vec2
+    {
+        Vector2::new(x, y)
+    }
+    pub fn vec3(x: f32, y: f32, z: f32) -> Vec3
+    {
+        Vector3::new(x, y, z)
+    }
 }
-pub fn vec3(x: f32, y: f32, z: f32) -> Vec3
-{
-    Vector3::new(x, y, z)
-}
-
 
 pub mod mesh_builder;
 pub mod ids;
@@ -33,3 +34,4 @@ pub use crate::dynamic_mesh::mesh::*;
 pub use crate::dynamic_mesh::mesh_builder::*;
 pub use crate::dynamic_mesh::ids::*;
 pub use crate::dynamic_mesh::traversal::*;
+pub use crate::math::*;
